6112d3a91f36de8dded01e5f6d488acc15117306
Mohammad Talim <talimmohammad116@gmail.com>
2024-07-17 11:01:09 +0530

    feat: build all schools page for showing school data

---
+++ app/schools/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { schools } from "./data";
+import "./page.css";
+
+interface Props {
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  contact: string;
+  image: string;
+  email: string;
+}
+
+function SchoolCard({ name, address, city, image }: Props) {
+  return (
+    <article className="school-card">
+      <Image
+        src={image}
+        alt={name}
+        width={256}
+        height={256}
+        className="school-image"
+      />
+      <div className="info">
+        <h3 className="truncate">{name}</h3>
+        <p>{address}</p>
+        <p>{city}</p>
+      </div>
+    </article>
+  );
+}
+
+export default function SchoolsPage() {
+  return (
+    <main className="container">
+      <h1>All Schools</h1>
+
+      <div className="school-grid">
+        {schools.map((school, index) => (
+          <SchoolCard key={index} {...school} />
+        ))}
+      </div>
+    </main>
+  );
+}
